be2884691fc9c16396c8114c03fd49e20e469811
Malik Piara <malikpiara@gmail.com>
2022-11-23 19:07:49 +0100

    Updated postgres port in datasource.

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ import { Comment } from "./entity/Comment";
 export const myDataSource = new DataSource({
     type: "postgres",
     host: process.env.POSTGRES_WRITER_HOST,
-    port: process.env.POSTGRES_PORT,
+    port: 5432,
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
